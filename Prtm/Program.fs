2652387f643e5f6c26c7fbb5c12c2c205c026d68
Dave Warry <dave@warry.org.uk>
2012-11-13 07:41:57 +0000

    Problem PRTM

---
+++ Prtm/Program.fs
@@ -0,0 +1,13 @@
+﻿// http://rosalind.info/problems/prot
+open System.IO
+open Rna
+open AminoAcids
+
+[<EntryPoint>]
+let main argv = 
+    use f = File.OpenText "rosalind_prtm.txt"
+    let mass = f.ReadLine() |> proteinStringToSeq |> calculateMassOfProteinSeq
+    
+    use output = File.CreateText "result.txt"
+    printfn "%0.2f" mass
+    0 // return an integer exit code
